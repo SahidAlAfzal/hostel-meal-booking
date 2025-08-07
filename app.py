37a0d10071bd4ec4d3d1527cdface5b61d58a262
sahid al afzal <sahidalafzal@Sahids-MacBook-Air.local>
2025-08-08 01:15:29 +0530

    ok

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ st.markdown("""
 
         /* WARNING alert - yellow */
         .st-fz {
-            background-color: rgba(255, 193, 7, 0.82) !important;
+            background-color: rgba(255, 193, 7, 0.9) !important;
             border: 1px solid #856404;
             color: white;
         }
